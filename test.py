ac1749dda69f6ac59a8cbcf5282f1f73cd954d6a
343829084 <343829084@qq.com>
2017-06-29 11:19:10 +0800

    Update test.py
    整理一下代码

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 #coding:utf-8
 
 import time
+import datetime
 import numpy as np
 import pandas as pd
 import matplotlib
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
 %matplotlib inline # 用于将图相钳到notebook中
+from mpl_toolkits.mplot3d import Axes3D
+import itertools
+import os
+import re
+from collections import defaultdict
 
 FIG_STYLE="bmh"
 #print plt.style.available
+
+matplotlib.rcParams['font.family'] = 'sans-serif'
+matplotlib.rcParams['font.sans-serif'] = ['SimHei'] + matplotlib.rcParams['font.sans-serif']
+matplotlib.rcParams['axes.unicode_minus'] = False
+
 matplotlib.style.use(FIG_STYLE)
 FIG_SIZE=(10,8)
 FIG_DPI=150
@@ -22,9 +33,54 @@ def describe(a):
     'mean': a.mean(),
     '50%': np.percentile(a, 50),
     '90%': np.percentile(a, 90),
+    '95%': np.percentile(a, 95),
+    '99%': np.percentile(a, 99),
     })
 
-#用表格统计latency值在不同段的分布
+def describe(a, b):
+    return pd.Series({
+    'count': len(a),
+    'bytes': sum(b),
+    'max': a.max(),
+    'std': a.std(),
+    'mean': a.mean(),
+    '50%': np.percentile(a, 50),
+    '90%': np.percentile(a, 90),
+    '95%': np.percentile(a, 95),
+    '99%': np.percentile(a, 99),
+    })
+
+def ReadCsvFileToDatas():
+    # 将csv文件内容读到Datas 这个list
+    internal_latency = pd.read_csv('test.csv', parse_date=True, usecols=['datetime','latency','len'])
+    internal_latency['datetime']=pd.to_datetime(internal['datetime'],utc=8)
+    internal_latency=internal_latency[internal_latency['latency']>0]
+    
+    datas=list()
+    if not internal_latency.empty:
+        datas.extend([
+            ('snap', internal_latency[['datetime','latency','len']]),
+        ])
+        
+ def ReadCsvFileToDatasOrderTime():
+    # 将csv文件内容读到Datas 这个list
+    internal_latency = pd.read_csv('test.csv', parse_date=True, usecols=['datetime','latency','len'])
+    internal_latency['datetime']=pd.to_datetime(internal['datetime'],utc=8)
+    internal_latency=internal_latency[internal_latency['latency']>0]
+    
+    datas=list()
+    datetimes = sorted(list(set(internal_latency['datetime'])))
+    tmp = list()
+    
+    for n in range(len(datetimes)):
+        tmp.append(pd.DataFrame(internal_latency[internal_latency['datetime']==datetimes[n]],
+                               columns=['datetime','latency','len']))
+        tmpdate = datetime.datetime.strptime("%d"%(datetimes[n]/1000), "%Y%m%d%H%M%S")
+        datas.extend([
+            ('%02d：%02d:%02d'%(tmpdate.hour, tmpdate.minute, tmpdate.second), tmp[n][['latency','len']]),
+        ])
+
+#用表格统计latency值在不同段的分布,没有bytes字段
 def getSummary():
     summary = pd.DataFrame()
     for name, data in datas:
@@ -33,10 +89,22 @@ def getSummary():
         summary = summary.append(s)
         summary = summary[['count','max','std','mean','50%','90%']]
      summary
+    
+    
+   #用表格统计latency值在不同段的分布,没有bytes字段
+ def getSummaryIncludeByte():
+    summary = pd.DataFrame()
+    for name, data in datas:
+        s = describe(data['Latency','len'])
+        s.name = name
+        summary = summary.append(s)
+        summary = summary[['count','bytes','max','std','mean','50%','90%']]
+     summary
             
- #给下面summary进行画图
+ #给下面summary进行简单画图
  def drawSummary():
     fig=plt.figure(figsize=FIG_SIZE, dpi=FIG_DPI)
+    
     ax=fig.add_subplot(111)
     summary.drop(['count','max','min'], axis=1).plot(ax=ax, market='o')
     plt.ylabel("microseconds")
@@ -53,50 +121,43 @@ def getSummary():
     summary2=summary
     summary.drop(['count','max','min'], axis=1).plot(ax=ax, market='o')
     summary2.drop(['count','max','std','50%','90%'], axis=1).plot(ax=ax2, market='o',color='black')
-    plt.ylabel("microseconds")
+    a1.set_ylabel(u'时延')
+    a2.set_ylabel(u'大小')
+    
+    
     plt.minorticks_on()
+    locs, labels = plt.xticks()
+    plt.setp(labels, rotation=20)
+    
     ax.legend(loc='upper center', ncol=len(summary.columns), bbox_to_anchor=(0.5, 1.10))
     ax2.legend(loc='lower center', ncol=len(summary2.columns), bbox_to_anchor=(0.5, 1.10))
     plt.savefig("summary.png")
     
     
-    #多个子图，共用一个X轴
+    #大小，时延，个数多个子图，共用一个X轴
    def drawCommonX():
     fig=plt.figure(figsize=FIG_SIZE, dpi=FIG_DPI)
-    ax1=fig.add_subplot(211)
-    ax2=fig.add_subplot(212)
+    fig.subplots_adjust(left=None,botton=None,right=None,top=None,hsapce=None,wspace=None)
+    ax1=fig.add_subplot(311)
+    ax2=fig.add_subplot(312)
+    ax3=fig.add_subplot(313)
     plt.subplots_adjust(0,0,1,1,0,0)
     summary2=summary
-    summary.drop(['count','max','min'], axis=1).plot(ax=ax1, market='o', sharex=True)
-    summary2.drop(['count','max','std','50%','90%'], axis=1).plot(ax=ax2, market='o',color='black')
-    plt.ylabel("microseconds")
+    summary3=summary
+    summary.drop(['count','max','bytes'], axis=1).plot(ax=ax1, market='o', sharex=True)
+    summary2.drop(['bytes','max','min','std','mean','50%','90%','95%','99%'], axis=1).plot(ax=ax2, market='o',color='red', sharex=True)
+    summary3.drop(['count','max','min','std','mean','50%','90%','95%','99%'], axis=1).plot(ax=ax3, market='o',color='red')
+    ax1.set_ylabel(u'时延')
+    ax2.set_ylabel(u'个数')
+    ax3.set_ylabel(u'大小')
+   
     plt.minorticks_on()
+    locs, labels = plt.xticks()
+    plt.setp(labels, rotation=20)
     ax.legend(loc='upper center', ncol=len(summary.columns), bbox_to_anchor=(0.5, 1.10))
     #ax2.legend(loc='lower center', ncol=len(summary2.columns), bbox_to_anchor=(0.5, 1.10))
     plt.savefig("summary.png")
  
-
-            
-#设置datas的值 
-def setDatas():            
-    tmp=list()
-    for n in range(10):
-        tmp.append(pd.DataFrame(internal[interanl['XX']==10], columns=['aa','bb']))
-        datas.extend([
-                    ('abc', tmp[n][['aa','bb']])，
-        ])
-        
-  #对行或列进行切片
- def cutData():
-     #对行切
-    df.iloc[1:3,;]
-    #对列切
-    df.iloc[:, 1:3]
-    #标签切片
-    df.loc['2001':'2004', ['a','b']]
-    
-            
-           
 #对于需要进行分组处理的图，如需要显示1秒（分成10分），每分内各种属性的点有多少个
 def analysis_group_data():
     fig=plt.figure(figsize=FIG_SIZE, dpi=FIG_DPI)
@@ -129,6 +190,34 @@ def analysis_group_data():
         data=tmp.loc[num]#得到某一组下的数据
         ax.plot(data, label=num, market='.', linestyle='', markersize=6) #linestype用于表示是用连线‘-’，markersize表点的大小
     ax.legend(loc='upper right', ncol=(len(num_list)+1)/5) #loc表示label写在什么方向，ncol表示要显示为多少列
+    
+    loc, labels = plt.xticks()
+    plt.ylabel(u'时延')
+    plt.xlabel(u'等分')
+    ax.legend(loc='upper right', ncol=1)
+    
+ ##以下为一些特性画图*********************************************************************           
+#设置datas的值 
+def setDatas():            
+    tmp=list()
+    for n in range(10):
+        tmp.append(pd.DataFrame(internal[interanl['XX']==10], columns=['aa','bb']))
+        datas.extend([
+                    ('abc', tmp[n][['aa','bb']])，
+        ])
+        
+  #对行或列进行切片
+ def cutData():
+     #对行切
+    df.iloc[1:3,;]
+    #对列切
+    df.iloc[:, 1:3]
+    #标签切片
+    df.loc['2001':'2004', ['a','b']]
+    
+            
+           
+
             
 def opencsvfile():
     internal=pd.read_csv('xx.csv', parse_dates=True)
